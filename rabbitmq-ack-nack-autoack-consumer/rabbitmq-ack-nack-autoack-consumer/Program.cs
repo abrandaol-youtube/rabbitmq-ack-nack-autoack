a6ada5ac248f77dbcfe765c0eb4f455dfa0d7c29
Alexandre Brandão lustosa <alexandre.brandao.lustosa@gmail.com>
2020-08-07 23:39:52 -0300

    Ajsutes

---
+++ rabbitmq-ack-nack-autoack-consumer/rabbitmq-ack-nack-autoack-consumer/Program.cs
@@ -0,0 +1,44 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace rabbitmq_ack_nack_autoack_consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                BuildAndRunWorker(channel, "Worker A");
+                
+                Console.ReadLine();
+            }
+        }
+
+        private static void BuildAndRunWorker(IModel channel, string workerName)
+        {
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($"{channel.ChannelNumber} - {workerName}: [x] Received {message}");
+            };
+
+            channel.BasicConsume(queue: "order", autoAck: true, consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+        }
+    }
+}
